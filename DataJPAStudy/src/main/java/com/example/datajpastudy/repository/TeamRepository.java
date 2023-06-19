25a6e0fa672e2de0bdd435b724e404b393495897
rhrudska987 <59828706+rhrudska987@users.noreply.github.com>
2023-06-19 18:54:51 +0900

    Delete DataJPAStudy directory

--- DataJPAStudy/src/main/java/com/example/datajpastudy/repository/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.datajpastudy.repository;
-
-import com.example.datajpastudy.entity.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
