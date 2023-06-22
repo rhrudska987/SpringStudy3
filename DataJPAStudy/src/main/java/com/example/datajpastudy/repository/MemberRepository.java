0cc99dacaeccb596b36dce4b230630b4490236be
rhrudska987 <rhrudska987@naver.com>
2023-06-22 17:23:15 +0900

    2023.06.22 study

---
+++ DataJPAStudy/src/main/java/com/example/datajpastudy/repository/MemberRepository.java
@@ -0,0 +1,58 @@
+package com.example.datajpastudy.repository;
+
+import com.example.datajpastudy.dto.MemberDto;
+import com.example.datajpastudy.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.repository.query.Param;
+
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
+
+    List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+    List<Member> findHelloBy();
+
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
+
+    @Query("select new com.example.datajpastudy.dto.MemberDto(m.id, m.username, t.name) from Member m join m.team t")
+    List<MemberDto> findMemberDto();
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findNames(@Param("names") Collection<String> names);
+
+    List<Member> findListByUsername(String username);
+    Member findMemberByUsername(String username);
+    Optional<Member> findOptionalByUsername(String username);
+
+    @Query(value = "select m from Member m left join m.team t", countQuery = "select count (m) from Member m")
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    @Modifying
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true")) //요즘은 이거 없어도 성능 잘 나옴
+    Member findReadOnlyByUsername(String username);
+
+    @Query(value = "select * from member where username = ?", nativeQuery = true)
+    Member findByNativeQuery(String username);
+
+}
---
+++ SpringWorkSpace/validation/src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,26 @@
+package hello.itemservice.validation;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+@SpringBootTest
+public class MessageCodesResolverTest {
+
+    MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+
+    @Test
+    void messageCodesResolverObject(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+        Assertions.assertThat(messageCodes).containsExactly("required.item", "required");
+    }
+
+    @Test
+    void messageCodesResolverField(){
+        String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+
+    }
+
+}
