88b7d11eb170f5583cc553ce1ce3f24bb4cc2f64
rhrudska987 <rhrudska987@naver.com>
2023-07-27 16:01:05 +0900

    2023.07.26 study

--- SpringWorkSpace/exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
+++ SpringWorkSpace/exception/src/main/java/hello/exception/api/ApiExceptionV2Controller.java
@@ -1,14 +1,12 @@
 package hello.exception.api;
 
-import ch.qos.logback.core.net.SocketConnector;
 import hello.exception.exception.UserException;
-import hello.exception.exhandler.ErrorResult;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
